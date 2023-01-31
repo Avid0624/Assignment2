4e5766efc75534fe52bc79bf342a6f86e9d4f85b
Alexandra DeLeon <123036613+Avid0624@users.noreply.github.com>
2023-01-31 14:07:56 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,16 @@ using namespace std;
 int main(){
   cout<<"Hello World"<<endl;
 }
+
+int main(int argc , char ** argv){
+    cout<<"My Command line analyzer...."<<endl;
+    cout<<"The number of command line parameters are: "<<argc<<endl;
+    cout<<"The length of the name of the program is: "<<string_length(argv[0])<<endl;
+    if(argc > 1){
+
+        for(int i = 1 ; i < argc ; i++){
+            cout<<"The length of the parameter "<<i<<" is "<<string_length(argv[i])<<endl;
+        }
+
+    }
+}
