7702857bbdd799ff8da7e4113bbb0ed44bb7c680
Alexandra DeLeon <123036613+Avid0624@users.noreply.github.com>
2023-01-31 14:03:04 -0600

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,11 @@
+/*********************
+Name: Alexandra DeLeon
+COSC-2436-004
+**********************/
+
+#ifndef COMMANDLINE_FUNTIONS_H
+#define COMMANDLINE_FUNTIONS_H
+
+int string_length(const char *str);
+
+#endif
